c782585cbbf5d0278c0750a32884b9875a9ffeb8
Mazy <landonp95@gmail.com>
2022-05-19 23:39:22 -0400

    more tests

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,29 @@
-describe('Navigation', () => {
-  it('should navigate to the skills page', () => {
-    cy.visit('/')
-    cy.get('a[href*="skills"]').click()
-    cy.url().should('include', '/skills')
-  })
-})
+describe("Navigation", () => {
+  it("should navigate to the skills page", () => {
+    cy.visit("/");
+    cy.get('a[href*="skills"]').click();
+    cy.url().should("include", "/skills");
+  });
+  it("should navigate to the projects page", () => {
+    cy.visit("/");
+    cy.get('a[href*="projects"]').click();
+    cy.url().should("include", "/projects");
+  });
+  it("should navigate to the about page", () => {
+    cy.visit("/");
+    cy.get('a[href*="about"]').click();
+    cy.url().should("include", "/about");
+  });
+});
+
+describe("Error handling", () => {
+  it("should go to a 404 page", () => {
+    cy.visit({
+      url: "/fitnessgrampacertest",
+      method: "GET",
+      failOnStatusCode: false,
+    });
+    cy.url().should("include", "/fitnessgrampacertest");
+    cy.get("h2").contains("This page could not be found.");
+  });
+});
