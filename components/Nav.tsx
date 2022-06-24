48ae99be51855e7c3d9404f9ec178d0f819638e7
Mazy <landonp95@gmail.com>
2022-06-24 00:26:18 -0400

    fix the space between nav buttons being clickable

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,22 +8,22 @@ export default function Nav() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg z-16">
       <Link href="/">
-        <a>
+        <a className="mx-2 w-0">
           <SideBarIcon icon={<BsFillHouseFill size="32" />} text="Home" />
         </a>
       </Link>
       <Link href="/skills">
-        <a>
+        <a className="mx-2 w-0">
           <SideBarIcon icon={<GiOpenBook size="32" />} text="Skills" />
         </a>
       </Link>
       <Link href="/projects">
-        <a>
+        <a className="mx-2 w-0">
           <SideBarIcon icon={<FaCode size="32" />} text="Projects" />
         </a>
       </Link>
       <Link href="/about">
-        <a>
+        <a className="mx-2 w-0">
           <SideBarIcon icon={<BsPersonFill size="32" />} text="About Me" />
         </a>
       </Link>
